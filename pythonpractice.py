c9a6ad0bed77f2738dd9699c8a0b5ea54f09fcbf
vitaminxxc <pankajchoudharyy10@gmail.com>
2025-05-17 23:15:04 -0400

    Add files via upload

---
+++ pythonpractice.py
@@ -0,0 +1,23 @@
+# task1 : basic Mathematical Operations
+x = int(input("Enter the first number:"))
+y = int(input("Enter the second number:"))
+
+a = x + y
+b = x - y
+c = x * y
+d = x / y
+
+print("Addition:", a)
+print("Substraction:", b)
+print("Multiplication:", c)
+print("Division:", d)
+
+
+
+# task2 : personalized greeting
+p = str(input("Enter your first name:"))
+q = str(input("Enter your last name:"))
+
+r = p + " " +  q
+
+print("Hello,",r + "! Welcome to the python language")
